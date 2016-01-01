6c867a3370baf09d6bc78109830d8f1c74bf62dd
Hedy <hedywoo@gmail.com>
2016-01-01 16:09:09 -0700

    finished metrics_calcs

--- lib/metrics_calculator.rb
+++ lib/metrics_calculator.rb
@@ -1,3 +1,32 @@
 class MetricsCalculator
 
+  def initialize
+    @section_total = []
+    @section_grade = []
+  end
+
+  def add(sections)
+    @section_total << sections
+  end
+
+  def add_grade(grades)
+    @section_grades << grades
+  end
+
+  def section_count
+    @section_total.count
+  end
+
+  def total_students
+    @section_total.map {|x| x.students_count}.inject(:+)
+  end
+
+  def average_grade
+    students_array = @section_total.map {|x| x.students_count}
+    grades_array =@section_total.map {|y| y.grade}
+    combo_arrays = students_array.zip(grades_array)
+    total_grade = combo_arrays.map {|x| x[0] * x[1]}.inject(:+) / total_students.to_f
+    total_grade.round(1)
+  end
+
 end
--- test/metrics_calculator_test.rb
+++ test/metrics_calculator_test.rb
@@ -12,6 +12,7 @@ class MetricsCalculatorTest < Minitest::Test
     sec_1 = Section.new("Biology 1")
     sec_2 = Section.new("Biology 2")
     sec_3 = Section.new("Biology 3")
+
     calc.add(sec_1)
     calc.add(sec_2)
     calc.add(sec_3)
@@ -19,7 +20,6 @@ class MetricsCalculatorTest < Minitest::Test
   end
 
   def test_it_finds_a_total_number_of_students
-    skip
     calc = MetricsCalculator.new
     sec_1 = Section.new("Biology 1")
     sec_2 = Section.new("Biology 2")
@@ -28,23 +28,35 @@ class MetricsCalculatorTest < Minitest::Test
     calc.add(sec_2)
     calc.add(sec_3)
     # sec 1 should have 22 students
+    sec_1.stubs(:students_count).returns(22)
     # sec 2 should have 24 students
+    sec_2.stubs(:students_count).returns(24)
     # sec 3 should have 18 students
+    sec_3.stubs(:students_count).returns(18)
     assert_equal 64, calc.total_students
   end
 
   def test_it_calculates_an_average_grade
-    skip
     calc = MetricsCalculator.new
     sec_1 = Section.new("Biology 1")
     sec_2 = Section.new("Biology 2")
     sec_3 = Section.new("Biology 3")
+    # grade_1 = mock('grade')
+    # grade_2 = mock('grade')
+    # grade_3 = mock('grade')
+    # calc.add_grade
+
     calc.add(sec_1)
     calc.add(sec_2)
     calc.add(sec_3)
-    # sec 1 should have an average grade of 82 and 4 students
-    # sec 2 should have an average grade of 86 and 5 students
-    # sec 3 should have an average grade of 90 and 6 students
+
+    sec_1.stubs(:students_count).returns(4)
+    sec_1.stubs(:grade).returns(82)
+    sec_2.stubs(:students_count).returns(5)
+    sec_2.stubs(:grade).returns(86)
+    sec_3.stubs(:students_count).returns(6)
+    sec_3.stubs(:grade).returns(90)
+# binding.pry
     assert_equal 86.5, calc.average_grade
   end
 end
